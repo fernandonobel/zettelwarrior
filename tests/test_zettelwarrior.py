ff8c04fc298fd8b34095e35eab0acc64c6c09d01
FernandoNobel <fersann1@upv.es>
2022-06-17 11:19:17 +0200

    Rename some functions

--- tests/test_zettelwarrior.py
+++ tests/test_zettelwarrior.py
@@ -7,25 +7,20 @@ from zettelwarrior.zettel import Zettel
 from zettelwarrior.zettelkasten import Zettelkasten
 
 @pytest.fixture
-def zettelkasten_path(tmpdir):
-
+def tmp_zettelkasten_dir(tmpdir):
     result = Path(tmpdir)
-
     copy_example_files_into(result)
-
     return result
 
-def copy_example_files_into(path):
 
+def copy_example_files_into(path):
     copy_tree("./examples/", str(path))
 
 
-def test_zettelkasten(zettelkasten_path):
+def test_zettelkasten(tmp_zettelkasten_dir):
 
-    zettelkasten = Zettelkasten(zettelkasten_path)
-
+    zettelkasten = Zettelkasten(tmp_zettelkasten_dir)
     expected = [Zettel().load("./examples/220612-1235.md")]
-
     assert zettelkasten.zettels == expected
 
 
@@ -33,9 +28,7 @@ def test_tags():
 
     zettelkasten = Zettelkasten("./examples/")
     result = zettelkasten.tags()
-
     expected = {"example-note": [None], "test": [None]}
-
     assert result == expected
 
 
@@ -44,9 +37,7 @@ def test_new_uuid():
     fake_now = datetime.datetime(2022, 6, 15, 20, 11, 55)
     zettelkasten = Zettelkasten("./examples/")
     result = zettelkasten.generate_new_uuid(fake_now)
-
     expected = "220615-2011"
-
     assert result == expected
 
 
