4f7fbcfa73122a1189de2d3ed5322ae300f2d87e
“fernandonobel” <fersann1@upv.es>
2022-06-23 13:34:50 +0200

    Add config.py and tests for it

---
+++ src/zettelwarrior/config.py
@@ -0,0 +1,25 @@
+import configparser
+from dataclasses import dataclass
+from pathlib import Path
+
+import os
+
+
+@dataclass
+class Config:
+    """Class for handling the configuration options of
+    ZettelWarrior."""
+
+    path: Path = None
+
+    def user_home_path(self):
+        return Path(os.path.expanduser('~'))
+
+    def load_user_config(self):
+        config_path = self.user_home_path() / '.zwrc'
+        self.load_config_file(config_path)
+
+    def load_config_file(self, config_path):
+        config_file = configparser.ConfigParser() 
+        config_file.read(config_path)
+        self.path = Path(config_file['zettelwarrior']['path'])
